cb0167dcdc7ed6fc5ac1ac49c9d5cefbd0fc1ae7
Cedric Stark <cedric.stark@tum.de>
2021-01-16 13:13:42 +0100

    Update download link to dropbox

--- scripts/getModelWeights.sh
+++ scripts/getModelWeights.sh
@@ -3,4 +3,4 @@
 mkdir model_weights
 mkdir model_weights/pretrained
 
-wget https://drive.google.com/file/d/1inwkeaUBXXWc4JOtuFtjzw3Aavhe7yUN/view?usp=sharing
+wget https://www.dropbox.com/s/skt0ga03vjqrfo1/best_GEN.pth?dl=0 -O model_weights/pretrained/best_GEN.pth
